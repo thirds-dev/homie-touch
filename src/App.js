4d9da9dda5f1929ebc9969cd2b2e97e5b3b7ebd9
Ryan Campbell <ryan@ryancampbell.com>
2022-06-13 20:04:58 -0700

    use grommet

--- src/App.js
+++ src/App.js
@@ -1,31 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Box,
+  Button,
+  Grommet,
+  Text,
+  TextInput,
+  ResponsiveContext
+} from "grommet";
+import { Search, Menu } from "grommet-icons";
+import { theme } from "./theme";
+import {
+  AppHeader,
+  Hardware,
+  Notification,
+  UtilizationCard,
+  VirtualMachinesCard
+} from "./components";
+import { hardware, utilization, vms, notification } from "./data";
+const userSession = {
+  user: {
+    name: "Shimi Shimi",
+    thumbnail: "//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80"
+  },
+  items: [
+    {
+      label: "Logout",
+      href: "#"
+    }
+  ]
+};
 
-function App() {
-  const tellAJoke = async (e) => {
-    await fetch('http://api.homie/joke/random');
-  };
-  const startTheParty = async (e) => {
-    await fetch('http://api.homie/party/start');
-  };
-  const stopTheParty = async (e) => {
-    await fetch('http://api.homie/party/stop');
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Homie Touch
-        </p>
-        <p>
-          <button onClick={tellAJoke}>Tell a Joke</button>
-          <button onClick={startTheParty}>Start the Party</button>
-          <button onClick={stopTheParty}>Stop the Party</button>
-        </p>
-      </header>
-    </div>
-  );
+class AppBody extends Component {
+  static contextType = ResponsiveContext;
+
+  render() {
+    return (
+      <Grommet theme={theme} full>
+        <Box fill background="light-3">
+          <AppHeader
+            appName="Yo, Homie!"
+            appIcon={<Menu />}
+            userSession={userSession}
+          />
+          <Box flex overflow="auto" gap="medium" pad="medium">
+            <Box
+              flex={false}
+              overflow="auto"
+              round="large"
+              background={{ color: "dark-5", opacity: "weak" }}
+              direction="row"
+              align="center"
+              pad={{ horizontal: "medium", vertical: "small" }}
+              margin={{ horizontal: "medium", top: "medium" }}
+            >
+              <Search color="brand" />
+              <TextInput plain placeholder="Search Cluster" type="search" />
+            </Box>
+            <Box flex={false} direction="row-responsive" wrap>
+              <Box gap="large" flex="grow" margin="medium">
+                <Notification data={notification} />
+                <VirtualMachinesCard data={vms} />
+              </Box>
+              <Box gap="large" flex="grow" margin="medium">
+                {utilization.map(data => (
+                  <UtilizationCard key={data.name} data={data} />
+                ))}
+              </Box>
+              <Box flex="grow" margin="medium">
+                <Hardware data={hardware} />
+              </Box>
+            </Box>
+          </Box>
+
+          <Box
+            flex={false}
+            pad={{ vertical: "xsmall", left: "medium" }}
+            responsive={false}
+            background={{ color: "brand", dark: false }}
+            direction="row"
+            align="center"
+            justify="between"
+          >
+            <Text color="light-1">5 Running Tasks</Text>
+            <Button
+              size="medium"
+              label="Show Tasks"
+              icon={<Menu color="white" />}
+              reverse={true}
+            />
+          </Box>
+        </Box>
+      </Grommet>
+    );
+  }
 }
+
+const App = () => (
+  <Grommet theme={theme} full>
+    <AppBody />
+  </Grommet>
+);
 
 export default App;
